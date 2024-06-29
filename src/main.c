7950a01e4e8dca584ac9912383084b0941335020
Nadhem BENAMARA <nadhem.benamara-extern@renault.com>
2024-06-29 14:35:26 +0200

    add struct type

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main() 
+    typedef struct {
+        int a;
+        char b;
+        char* str;
+    } identity_t;
+
+int main()
 {
+    identity_t cin ;
+    cin.a = 3;
+
     printf("hello world !");
     return 0;
 }
